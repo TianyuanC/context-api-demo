07fa6ec65253371e1beefcec7a243a9ede0e8c37
Ryan Chu <tianyuan.chu@gmail.com>
2019-04-20 12:23:23 -0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { LocaleContext, ThemeContext } from "./context";
+
+const Section = () => {
+    return (
+        <div>
+            {/* <NestedControl theme={props.theme} /> */}
+            <NestedControl />
+        </div>
+    );
+};
+
+class NestedControl extends React.Component {
+    static contextType = ThemeContext;
+    render() {
+        // return <p>{this.props.theme} theme</p>;
+        return (
+            <>
+                <p>{this.context} theme</p>
+                <LocaleContext.Consumer>
+                    {value => <p>locale: {value}</p>}
+                </LocaleContext.Consumer>
+            </>
+        );
+    }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <LocaleContext.Provider value="en">
+                        <ThemeContext.Provider value="light">
+                            <Section theme="dark" />
+                        </ThemeContext.Provider>
+                    </LocaleContext.Provider>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
